0dcc71378bed512c5e9068fd96e083da3129f474
akashxgithub <theog.cse.rymec@gmail.com>
2025-02-01 23:57:01 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useReducer, useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
+
+const Navbar = () => {
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+
+  const navContainerRef = useRef(null);
+  const audioElementRef = useRef(null);
+
+  const toggleAudioIndicator = () => {
+    setIsAudioPlaying((prev) => !prev);
+    setIsIndicatorActive((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isAudioPlaying) {
+      audioElementRef.current.play();
+    } else {
+      audioElementRef.current.pause();
+    }
+  }, [isAudioPlaying]);
+
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transiton-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="logo" className="w-10" />
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-100 md:flex hidden items-center justify-center gap-1"
+            />
+          </div>
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item) => (
+                <a
+                  key={item}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+            <button
+              className="ml-10 flex iems-center space-x-0.5"
+              onClick={toggleAudioIndicator}
+            >
+              <audio
+                src="/audio/loop.mp3"
+                ref={audioElementRef}
+                className="hidden"
+                loop
+              />
+                {[1, 2, 3].map((bar) => (
+                  <div
+                    key={bar}
+                    className={`indicator-line ${
+                      isIndicatorActive ? "active" : ""
+                    }`}
+                    style={{ animationDelay: `${bar * 0.2}s` }}
+                  />
+                ))}
+             
+            </button>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
